35913b39f8fe168ff763269b39e0e5f25e0c807a
tqdcmo <m.taghadosi72@gmail.com>
2025-02-02 22:41:05 +0330

    task1

--- src/main/java/amol/springfreamwork/spring6webapp/domain/MyController.java
+++ src/main/java/amol/springfreamwork/spring6webapp/domain/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/")
     public String home() {
+
         return "Hello,mohamad welcome to java and springfreamwork";
 
 
